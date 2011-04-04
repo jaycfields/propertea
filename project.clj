374ce94edf3e0ad9f2b1de4b52356fc141e7ef5d
Jay Fields <github@jayfields.com>
2011-04-04 19:08:10 -0400

    update v

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject propertea "1.0.0"
+(defproject propertea "1.0.1"
   :description "load, coerce, and validate property files"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
-   :dev-dependencies [[expectations "1.0.0"]])
+  :dev-dependencies [[expectations "1.0.0"]
+                     [lein-clojars "0.6.0"]])
