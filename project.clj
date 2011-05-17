b6be8404f8f2eee5e75770fc1b7c39e9238a2e16
Jay Fields <github@jayfields.com>
2011-05-17 14:59:51 -0400

    new v

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject propertea "1.2.2"
+(defproject propertea "1.2.3"
   :description "load, coerce, and validate property files"
   :jar-name "propertea.jar"
   :dependencies [[org.clojure/clojure "1.2.0"]
